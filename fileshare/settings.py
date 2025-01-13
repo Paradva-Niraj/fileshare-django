74d7c93518e05cc8decad6484e29a14de969ace3
niraj <nirajparadva2004@gmail.com>
2025-01-13 21:46:56 +0530

    update login and logout view
    fix the login bug adding some code on setting file

--- fileshare/settings.py
+++ fileshare/settings.py
@@ -131,3 +131,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# login authentication 
+# settings.py
+
+LOGIN_URL = '/login/'  # Default login page
+LOGIN_REDIRECT_URL = '/dashboard/'  # Redirect after successful login
+LOGOUT_REDIRECT_URL = '/login/'  # Redirect after logout
+
--- sharefile/urls.py
+++ sharefile/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('',views.upload_file,name='upload_file'),
-    path('register',views.register_view,name='register_view'),
+    # path('',views.upload_file,name='upload_file'),
+    path('register/',views.register_view,name='register_view'),
     path('login/',views.login_view,name='login_view'),
-    path('dashboard/', views.dashboard_view, name='dashboard_view'),
+    path('', views.dashboard_view, name='dashboard_view'),
     path('logout/',views.logout_view, name='logout_view')
 ]
